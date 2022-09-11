227a1a81109450422416959108016281f303a31d
DongYuZhong <dongyzchn@gmail.com>
2022-09-11 23:06:51 +0800

    feat: 工程化配置

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off'
+  },
+  globals: {}
+}
