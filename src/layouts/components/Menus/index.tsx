abb3caebefae9bde352e317c62cd4cda137b8676
DongYuZhong <dongyzchn@gmail.com>
2022-09-18 23:05:02 +0800

    feat: add menu defaule open keys and default select keys

--- src/layouts/components/Menus/index.tsx
+++ src/layouts/components/Menus/index.tsx
@@ -1,5 +1,6 @@
 import { Menu, MenuProps } from 'antd'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { getParentIds } from '@/utils'
 
 interface props {
   menus: MenuProps['items']
@@ -7,6 +8,16 @@ interface props {
 
 const Menus = ({ menus }: props) => {
   const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const findDefaultOpenKeys = (key: string, routes: MenuProps['items']) => {
+    if (!routes || !routes.length) return []
+    return getParentIds(key, routes) || []
+  }
+  const defaultSelect = [pathname]
+  const defaultOpenKeys = findDefaultOpenKeys(pathname, menus)
+
+  console.log(defaultOpenKeys)
 
   const onSelect: MenuProps['onSelect'] = ({ key }) => {
     navigate({
@@ -14,7 +25,16 @@ const Menus = ({ menus }: props) => {
     })
   }
 
-  return <Menu items={menus} mode="inline" theme="dark" onSelect={onSelect} />
+  return (
+    <Menu
+      items={menus}
+      defaultOpenKeys={defaultOpenKeys}
+      defaultSelectedKeys={defaultSelect}
+      mode="inline"
+      theme="dark"
+      onSelect={onSelect}
+    />
+  )
 }
 
 export default Menus
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
-// 路径拼接
+/**
+ * 路径拼接
+ * @param baseUrl 路径前缀
+ * @param relativeUrl
+ */
 export const combineUrl = (baseUrl: string, relativeUrl: string) => {
   if (baseUrl.endsWith('/')) {
     baseUrl = baseUrl.slice(0, baseUrl.length - 1)
@@ -9,4 +13,23 @@ export const combineUrl = (baseUrl: string, relativeUrl: string) => {
   if (relativeUrl.startsWith('./')) return baseUrl + '/' + relativeUrl.slice(2, baseUrl.length)
 
   return baseUrl + '/' + relativeUrl
+}
+
+export const getParentIds = (id: string, tree: any[], fieldNames = { id: 'key', children: 'children' }) => {
+  function dfs(id: string, tree: any[], parents: string[] = []) {
+    for (let i = 0; i < tree.length; i++) {
+      const item = tree[i]
+      if (item[fieldNames.id] === id) return parents
+
+      if (!item[fieldNames.children] || !item[fieldNames.children].length) continue
+
+      // 向下寻找，先将id入栈
+      parents.push(item[fieldNames.id])
+      if (dfs(id, item[fieldNames.children], parents)?.length) return parents
+      parents.pop()
+    }
+    return []
+  }
+
+  return dfs(id, tree)
 }
